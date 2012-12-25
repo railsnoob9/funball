3a641c14a7003c22044e8b9369d261c8f92a4742
BLORGPULKFPLORKSICKLE <newal101587@gmail.com>
2012-12-25 14:07:26 -0800

    -basic example is working
    -run: "node app2ndattempt.js" from within the project folder...then go to http://localhost:8080/ and it should work

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var socket = require('socket.io');
+app.configure(function(){
+  app.use(express.static(__dirname + '/'));
+});
+var server = app.listen(8080);
+var io = socket.listen(server);
+
+io.sockets.on('connection', function (socket) {
+	console.log("connnect");
+	socket.on('disconnect', function (socket) {
+		console.log("disconnect");
+	});
+	socket.emit("pong",{txt:"Connected to server"});
+	socket.on('ping', function (data) {
+		console.log(data.txt);
+		socket.emit("pong",{txt:"Pong (from server)"});
+	});
+});
