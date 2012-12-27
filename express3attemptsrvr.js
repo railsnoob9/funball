dc71319e1be04253fd63a75fa6291d485bfd996c
Miles Gray <miles@resatiate.com>
2012-12-27 17:17:56 -0600

    unique addrress
    cleaned things up, added a new unique address and got an id from the
    socket

--- express3attemptsrvr.js
+++ express3attemptsrvr.js
@@ -8,14 +8,42 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/express3index.html');
 });
 
+io.set('log', 0);
+
+var games = [];
+
+var game = {
+  players: {
+    red: {
+      address: '0.0.0.0:0000',
+      position: {
+        x: 0,
+        y: 0
+      },
+      moves: []
+    },
+    blue: {
+      address: '0.0.0.0:0000',
+      position: {
+        x: 0,
+        y: 0
+      },
+      moves: []
+    }
+  },
+  index: 1
+};
+
 io.sockets.on('connection', function (socket) {
-	var address = socket.handshake.address;
-    console.log("New connection from " + address.address + ":" + address.port);
+    var address = socket.handshake.address;
+    var id = socket.id;
+    var unique_address = address.address + ":" + address.port;
+    console.log("New connection from " + address.address + ":" + address.port + ", with id " + id);
 
     socket.emit('news', { hello: 'client' });
     socket.on('my other event', function (data) {
-  		data.directionFromClient = data.directionFromClient + "     Oh shit, red just bumped into you -server";
+  		data.directionFromClient = data.directionFromClient;
   		data.another = "THIS IS ANOTHER THING on the server";
-    console.log(data);
-  });
+      console.log(data);
+    });
 });
